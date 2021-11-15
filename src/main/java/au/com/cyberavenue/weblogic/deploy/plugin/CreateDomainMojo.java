ab42c596c517d2603898974a6a6be7c6a2bd8054
Anthony <anthony.fryer@gmail.com>
2021-11-15 14:34:05 +1000

    initial commit

---
+++ src/main/java/au/com/cyberavenue/weblogic/deploy/plugin/CreateDomainMojo.java
@@ -0,0 +1,33 @@
+package au.com.cyberavenue.weblogic.deploy.plugin;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.BuildPluginManager;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "createDomain")
+public class CreateDomainMojo extends AbstractMojo {
+
+    @Parameter(property = "oracle.home", defaultValue = "${env.ORACLE_HOME}")
+    private String oracleHome;
+
+    @Component
+    private MavenProject mavenProject;
+
+    @Component
+    private MavenSession mavenSession;
+
+    @Component
+    private BuildPluginManager pluginManager;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("oracleHome = " + oracleHome);
+    }
+
+}
