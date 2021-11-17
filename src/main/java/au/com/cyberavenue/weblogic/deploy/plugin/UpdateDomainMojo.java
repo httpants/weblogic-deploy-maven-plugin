2b851cd3193460a7b39ecd0a0c4e4843b196c8ec
Anthony Fryer <anthony.fryer@energyq.com.au>
2021-11-17 20:44:37 +1000

    Using AbstractWeblogicDeployMojo for all mojos

--- src/main/java/au/com/cyberavenue/weblogic/deploy/plugin/UpdateDomainMojo.java
+++ src/main/java/au/com/cyberavenue/weblogic/deploy/plugin/UpdateDomainMojo.java
@@ -3,12 +3,12 @@ package au.com.cyberavenue.weblogic.deploy.plugin;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 
-@Mojo(name = "createDomain", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, requiresProject = false)
-public class CreateDomainMojo extends AbstractWeblogicDeployMojo {
+@Mojo(name = "updateDomain", defaultPhase = LifecyclePhase.PROCESS_RESOURCES, requiresProject = false)
+public class UpdateDomainMojo extends AbstractWeblogicDeployMojo {
 
     @Override
     String getWeblogicDeployCommand() {
-        return "createDomain";
+        return "updateDomain";
     }
 
 }
